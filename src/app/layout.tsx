172414572e6dfe8a2c8198787d1c25d9f053ef4f
idkhm0728 <idkhm0728@naver.com>
2025-01-21 11:09:03 +0900

    🔧 chore: 폰트 설정정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const freesentationFont = localFont({
+  src: "../../public/fonts/FreesentationVf.ttf",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${freesentationFont.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
