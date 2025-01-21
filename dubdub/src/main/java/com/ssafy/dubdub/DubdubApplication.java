6cfa43a56ea4bffa38e7f8b45d54575e2574ed08
이주은 <wndms058@gmail.com>
2025-01-21 21:33:28 +0900

    server init

---
+++ dubdub/src/main/java/com/ssafy/dubdub/DubdubApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dubdub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubdubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubdubApplication.class, args);
+	}
+
+}
