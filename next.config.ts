ca3b456050e6c9dcc41f4d5b66c82312d7dcbb49
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-01-21 13:54:47 +0900

    🐛 fix: svgr 라이브러리 관련 webpack config 오류 해결결

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
+  reactStrictMode: true,
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: ["@svgr/webpack"],
     });
+
+    return config;
   },
 };
 
