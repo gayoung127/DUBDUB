58fe9f1bf8550b0d1ea3994a36e9985480580123
gayoung127 <dinin1217@gmail.com>
2025-01-22 17:38:11 +0900

    ✨ Feat: 상세 조회 UI 구현 중

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -7,16 +7,17 @@ export default function RoomDetailPage() {
       <div>
         <Title />
         <DubbingDate />
-        <p>작성자</p>
       </div>
       <p>썸네일</p>
-      <p>방송 참여 인원</p>
       <p>모집 상태</p>
       <p>설명</p>
       <p>역할</p>
-      <p>장르</p>
-      <p>타입</p>
+      <div>
+        <p>장르</p>
+        <p>타입</p>
+      </div>
       <p>대본</p>
+      <button>신청하기</button>
     </div>
   );
 }
