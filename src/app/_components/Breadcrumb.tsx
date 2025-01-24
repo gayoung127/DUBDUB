222e2209c41dda500dee1525e1f1fb2a1fe56aed
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-01-24 10:19:48 +0900

    ✨ feat: Breadcrumb 라우팅 기능 구현현

--- src/app/_components/Breadcrumb.tsx
+++ src/app/_components/Breadcrumb.tsx
@@ -1,19 +1,34 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
 import BreadcrumbNextIcon from "@/public/images/icons/icon-breadcrumb-next.svg";
+import H4 from "./H4";
+import Link from "next/link";
 
 const BreadCrumb = () => {
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/").filter((segment) => segment);
+
+  let accumulatedPath = "";
+
   return (
     <div className="flex flex-1 flex-row items-center justify-start gap-3 px-5 py-6">
-      <h4 className="last:border-b-white text-base font-medium leading-none tracking-normal text-[#fefefe] last:border-b-2 last:border-white-100">
-        LOBBY
-      </h4>
-      <BreadcrumbNextIcon width={20} height={20} />
-      <h4 className="last:border-b-white text-base font-medium leading-none tracking-normal text-[#fefefe] last:border-b-2 last:border-white-100">
-        짱구 더빙하실 분
-      </h4>
-      <BreadcrumbNextIcon width={20} height={20} />
-      <h4 className="last:border-b-white text-base font-medium leading-none tracking-normal text-[#fefefe] last:border-b-2 last:border-white-100">
-        STUDIO
-      </h4>
+      {pathSegments.map((segment, index) => {
+        accumulatedPath += `/${segment}`;
+
+        return (
+          <React.Fragment key={segment}>
+            <Link href={accumulatedPath}>
+              <H4 className="min-h-5 text-white-100 last:border-b-2 last:border-b-white-100">
+                {segment.toUpperCase()}
+              </H4>
+            </Link>
+            {index < pathSegments.length - 1 && (
+              <BreadcrumbNextIcon width={20} height={20} />
+            )}
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
