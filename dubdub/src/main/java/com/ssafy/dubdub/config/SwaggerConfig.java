649a07206a10d42df004fe50598505e09a5a9886
j2j2 <fineimagination@naver.com>
2025-01-24 17:33:04 +0900

    env: 프로젝트 환경설정

---
+++ dubdub/src/main/java/com/ssafy/dubdub/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.dubdub.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "DubDub API",
+                description = """
+                        API Description
+                        """,
+                version = "v1.0.0"),
+        servers = @Server(url = "/"))
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+@Configuration
+public class SwaggerConfig {
+
+}
