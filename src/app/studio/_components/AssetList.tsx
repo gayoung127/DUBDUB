db80cd80b77d1051df5beda8c88a256e11de0da1
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-01-24 17:36:07 +0900

    ✨ feat: 커스텀 스크롤바 UI 구현현

--- src/app/studio/_components/AssetList.tsx
+++ src/app/studio/_components/AssetList.tsx
@@ -3,14 +3,25 @@ import AssetCard from "./AssetCard";
 
 const AssetList = () => {
   return (
-    <div className="flex h-full w-full flex-1 flex-wrap items-start justify-start gap-6 border border-gray-300 px-4 py-7">
-      <AssetCard />
-      <AssetCard />
-      <AssetCard />
-      <AssetCard />
-      <AssetCard />
-      <AssetCard />
-      <AssetCard />
+    <div className="h-full max-h-[431px] w-full border border-gray-300 py-7 pl-4 pr-3">
+      <div className="scrollbar flex h-full max-h-[393px] w-full flex-1 flex-wrap items-start justify-start gap-6 overflow-y-scroll">
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+      </div>
     </div>
   );
 };
