7526e68ee8085b8ef7535e418b86817657e2d46a
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-01-26 15:40:59 +0900

    ✨ feat: AssetCard 클릭 시 UI 변경 구현현

--- src/app/studio/_components/AssetCard.tsx
+++ src/app/studio/_components/AssetCard.tsx
@@ -3,9 +3,19 @@ import React from "react";
 import AssetIcon from "@/public/images/icons/icon-asset.svg";
 import C3 from "@/app/_components/C3";
 
-const AssetCard = () => {
+interface AssetCardProps {
+  isAssetSelected: boolean; // 선택 상태
+  onClick: () => void; // 클릭 이벤트 핸들러
+}
+
+const AssetCard = ({ isAssetSelected, onClick }: AssetCardProps) => {
   return (
-    <div className="flex flex-col items-start justify-center gap-y-2">
+    <div
+      onClick={onClick}
+      className={`box-border flex flex-col items-center justify-center gap-y-2 border ${
+        isAssetSelected ? "border-[#3668FF]" : "border-transparent"
+      } hover:border-[#3668FF]`}
+    >
       <AssetIcon width={60} height={60} />
       <C3 className="text-white-100">철수(이현정)_1</C3>
     </div>
