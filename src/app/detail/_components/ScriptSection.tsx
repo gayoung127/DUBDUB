75b12e56d1681690ea711a6a04581321c3f9c031
gayoung127 <dinin1217@gmail.com>
2025-01-26 23:03:24 +0900

    🔧 Chore: 배열 순회 고유한 key값  추가

--- src/app/detail/_components/ScriptSection.tsx
+++ src/app/detail/_components/ScriptSection.tsx
@@ -11,9 +11,13 @@ const ScriptSection = ({ scripts }: ScriptsProps) => {
     <section className="flex flex-col gap-2 p-1">
       <h1 className="pl-1 text-2xl font-bold tracking-[1px]">SCRIPT</h1>
       <div className="white-scrollbar flex h-[482px] w-[440px] flex-col gap-3 overflow-y-auto overflow-x-hidden">
-        {scripts.map((script) => (
-          <Script script={script} />
-        ))}
+        {scripts.map((script, id) => (
+          <Script key={script.role + id} script={script} />
+        )) : (
+          <div className="flex w-[420px] flex-row gap-3 rounded-[4px] bg-white-300 px-4 py-2 text-sm">
+            <p className="text-center text-bold">대본이 없습니다.</p>
+          </div>
+        )}
       </div>
     </section>
   );
