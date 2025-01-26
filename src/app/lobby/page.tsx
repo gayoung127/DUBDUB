d51092dd3e7b65284687f0a796edc637fbd20027
idkhm0728 <idkhm0728@naver.com>
2025-01-26 15:33:54 +0900

    💄 style : 레이아웃 조정정

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -5,6 +5,7 @@ import TabMenu from "./_components/TabMenu";
 import Button from "../_components/Button";
 import { useSearchParams } from "next/navigation";
 import DubRoomArea from "./_components/DubRoomArea";
+import Header from "../_components/Header";
 
 const LobbyPage = () => {
   const searchParams = useSearchParams();
@@ -15,22 +16,28 @@ const LobbyPage = () => {
   };
 
   return (
-    <div className="flex h-full w-full">
-      <div className="flex h-full flex-[2] items-center justify-center pl-3">
-        <Filter />
-      </div>
+    <div className="flex h-full flex-col gap-5">
+      <Header />
 
-      <div className="flex h-screen flex-[8] flex-col justify-center">
-        <div className="p-5">
-          <div className="flex w-full justify-between border-b border-gray-500 px-5 pb-3">
-            <TabMenu />
-            <Button onClick={() => handleCreateRoom()}>
-              + 더빙룸 생성하기
-            </Button>
+      <div className="flex h-full w-full flex-col gap-4">
+        <div className="flex h-full">
+          <div className="flex flex-[2] items-center justify-center pl-3">
+            <Filter />
           </div>
-          <div className="mt-5">
-            {tab === "all" && <DubRoomArea isAll={true} />}
-            {tab === "my" && <DubRoomArea isAll={false} />}
+
+          <div className="flex flex-[8] flex-col justify-center">
+            <div className="p-5">
+              <div className="flex w-full justify-between border-b border-gray-500 px-5 pb-3">
+                <TabMenu />
+                <Button onClick={() => handleCreateRoom()}>
+                  + 더빙룸 생성하기
+                </Button>
+              </div>
+              <div className="mt-5">
+                {tab === "all" && <DubRoomArea isAll={true} />}
+                {tab === "my" && <DubRoomArea isAll={false} />}
+              </div>
+            </div>
           </div>
         </div>
       </div>
