2e20c2040d3baa0619885dc61b1eb8b03b9acbf4
idkhm0728 <idkhm0728@naver.com>
2025-01-27 14:32:01 +0900

    💄 style : 버튼 컴포넌트 크기 설정정

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -6,12 +6,31 @@ interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
   className?: string;
+  small?: boolean;
+  large?: boolean;
 }
 
-const Button = ({ outline, onClick, children, className }: ButtonProps) => {
+const Button = ({
+  outline,
+  onClick,
+  children,
+  className,
+  small,
+  large,
+}: ButtonProps) => {
+  const sizeClass = small
+    ? "text-sm px-3 py-2"
+    : large
+      ? "text-lg px-6 py-3"
+      : "text-base px-4 py-2";
+
   return (
     <button
-      className={`rounded-[8px] px-4 py-2 ${outline ? `bg-white border-brand-200 text-brand-200 shadow-md hover:border hover:border-brand-200` : `bg-brand-200 text-white-100 hover:bg-brand-300`} ${className}`}
+      className={`rounded-[8px] ${sizeClass} ${
+        outline
+          ? "bg-white shadow-md border border-brand-200 text-brand-200 hover:border hover:bg-brand-100"
+          : "bg-brand-200 text-white-100 hover:bg-brand-300"
+      } ${className}`}
       onClick={onClick}
     >
       {children}
