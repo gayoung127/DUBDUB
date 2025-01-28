dd915e84365e798ca1e0cc51b180f3e40d72f709
j2j2 <fineimagination@naver.com>
2025-01-28 17:35:50 +0900

    feat: 사용자 인증정보 구현 클래스
    S12P11A801-19

---
+++ dubdub/src/main/java/com/ssafy/dubdub/member/dto/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.ssafy.dubdub.member.dto;
+
+import com.ssafy.dubdub.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails, OAuth2User {
+
+    private Member member;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public String getName() {
+        return member.getEmail();
+    }
+}
