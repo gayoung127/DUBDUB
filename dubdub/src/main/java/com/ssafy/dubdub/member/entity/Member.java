909fe9c23ba7d75015a575a22c08d70f7367bfb0
j2j2 <fineimagination@naver.com>
2025-01-28 16:52:05 +0900

    feat: Member엔터티 설계
    S12P11A801-62

---
+++ dubdub/src/main/java/com/ssafy/dubdub/member/entity/Member.java
@@ -0,0 +1,55 @@
+package com.ssafy.dubdub.member.entity;
+
+import com.ssafy.dubdub.Recruitment.entity.Recruitment;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String provider;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String refreshToken;
+
+    @Enumerated(EnumType.STRING)
+    private Position position;
+
+    private String profileUrl;
+
+    @Column(nullable = false)
+    private boolean isOk;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Recruitment> recruitments = new ArrayList<>();
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
