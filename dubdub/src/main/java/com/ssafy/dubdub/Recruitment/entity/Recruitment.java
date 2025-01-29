89b933d719e54787ed789898992cddfe22d4d15b
j2j2 <fineimagination@naver.com>
2025-01-29 13:55:36 +0900

    🚧Feat: 임시 모집글 엔티티 설계

---
+++ dubdub/src/main/java/com/ssafy/dubdub/Recruitment/entity/Recruitment.java
@@ -0,0 +1,13 @@
+package com.ssafy.dubdub.Recruitment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Recruitment {
+    @Id @GeneratedValue
+    private Long id;
+}
