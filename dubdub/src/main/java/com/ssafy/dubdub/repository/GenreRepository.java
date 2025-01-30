76e2666086a6bb0bfafafc4f59a4d8bac0f2ee93
이주은 <68546023+lizuAg@users.noreply.github.com>
2025-01-31 03:59:55 +0900

    ✨ feat: GenreRepository 작성

---
+++ dubdub/src/main/java/com/ssafy/dubdub/repository/GenreRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.dubdub.repository;
+
+import com.ssafy.dubdub.domain.entity.Genre;
+import com.ssafy.dubdub.enums.GenreType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    Optional<Genre> findByGenreName(GenreType genreName);
+}
