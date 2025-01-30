897d4d54aaec9bfe8e9d34e60b54b1602f3d0c4e
이주은 <68546023+lizuAg@users.noreply.github.com>
2025-01-31 04:00:09 +0900

    ✨ feat: RecruitmentRepository 작성

---
+++ dubdub/src/main/java/com/ssafy/dubdub/repository/RecruitmentRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.dubdub.repository;
+
+import com.ssafy.dubdub.domain.entity.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+}
