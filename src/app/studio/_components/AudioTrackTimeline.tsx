e84caaa522dc8f15814646b92cbe480be1226b6e
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-01-31 11:03:19 +0900

    🐛 fix: AudioBlock 픽셀 단위 변환

--- src/app/studio/_components/AudioTrackTimeline.tsx
+++ src/app/studio/_components/AudioTrackTimeline.tsx
@@ -22,14 +22,16 @@ const AudioTrackTimeline = ({
   audioContext,
 }: AudioTrackTimelineProps) => {
   return (
-    <div className="flex h-[60px] w-full flex-row items-center justify-start overflow-hidden border border-gray-300">
-      <div className="relative flex h-full w-full">
+    <div
+      className="flex h-[60px] flex-row items-center justify-start overflow-hidden border border-gray-300"
+      style={{ width: `${totalDuration * 80}px` }} // 전체 타임라인 길이 설정
+    >
+      <div className="relative flex h-full">
         {files.map((file, index) => {
-          const leftPosition = `${(file.startPoint / totalDuration) * 100}%`;
+          const leftPosition = `${file.startPoint * 80}px`; // px 단위로 변환
           const width = `${
-            ((file.duration - file.trimStart - file.trimEnd) / totalDuration) *
-            100
-          }%`;
+            (file.duration - file.trimStart - file.trimEnd) * 80
+          }px`; // px 단위로 변환
 
           return (
             <div
