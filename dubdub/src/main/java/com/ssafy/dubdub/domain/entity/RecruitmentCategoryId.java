a69d9f3d7c1318f2f8041d3d0a39d82653ff24c9
이주은 <wndms058@gmail.com>
2025-02-01 22:39:18 +0900

    🐛 fix: 복합키 클래스에 equals(), hashcode() 추가

--- dubdub/src/main/java/com/ssafy/dubdub/domain/entity/RecruitmentCategoryId.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/entity/RecruitmentCategoryId.java
@@ -4,17 +4,28 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecruitmentCategoryId implements Serializable {
 
     private Long recruitmentId;
     private Long categoryId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RecruitmentCategoryId that = (RecruitmentCategoryId) o;
+        return Objects.equals(recruitmentId, that.recruitmentId) && Objects.equals(categoryId, that.categoryId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recruitmentId, categoryId);
+    }
 }
