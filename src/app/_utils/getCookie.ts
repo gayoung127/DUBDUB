9bb50aeef9872d1de5f98844ed66be8046a6605b
gayoung127 <dinin1217@gmail.com>
2025-02-06 18:39:45 +0900

    🐛 Fix: getCookie util 오타 수정

--- src/app/_utils/getCookie.ts
+++ src/app/_utils/getCookie.ts
@@ -1,4 +1,4 @@
-const getCookie = (name: string): string | null => {
+export const getCookie = (name: string): string | null => {
   if (typeof document === "undefined") return null;
   const match = document.cookie.match(new RegExp(`(^|)${name}=([^;]+)`));
   return match ? match[2] : null;
