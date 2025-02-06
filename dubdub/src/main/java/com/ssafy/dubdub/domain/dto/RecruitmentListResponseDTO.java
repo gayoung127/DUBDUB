e848a5fba18fd989b6bce688523c01bf9a8cf6ed
j2j2 <fineimagination@naver.com>
2025-02-06 11:19:25 +0900

    🔨Refactor: 로비 모집글 Request, Response DTO 스키마 추가

--- dubdub/src/main/java/com/ssafy/dubdub/domain/dto/RecruitmentListResponseDTO.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/dto/RecruitmentListResponseDTO.java
@@ -1,5 +1,6 @@
 package com.ssafy.dubdub.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,15 +10,33 @@ import java.util.List;
 @Getter
 @Builder
 public class RecruitmentListResponseDTO {
+    @Schema(description = "모집글 id", example = "1")
     private Long id;
+
+    @Schema(description = "모집글 제목", example = "너의 이름은~~")
     private String title;
+
+    @Schema(description = "시작 시간", example = "1, 2")
     private LocalDateTime startTime;
+
+    @Schema(description = "종료 시간", example = "1, 2")
     private LocalDateTime endTime;
+
+    @Schema(description = "모집글 모집 여부", example = "true")
     private boolean isRecruiting;
+
+    @Schema(description = "모집글의 스튜디오 onAir 여부", example = "true")
     private boolean onAir;
+
+    @Schema(description = "현재 참가자 수", example = "1")
     private int currentParticipants;
+
+    @Schema(description = "필요한 참가자 수", example = "5")
     private int totalParticipants;
-    private Long authorId;
+
+    @Schema(description = "모집글 선택된 장르들", example = "1, 2")
     private List<Long> genres;
+
+    @Schema(description = "모집글 선택된 카테고리들", example = "1, 2")
     private List<Long> categories;
 }
--- dubdub/src/main/java/com/ssafy/dubdub/domain/dto/RecruitmentSearchRequestDTO.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/dto/RecruitmentSearchRequestDTO.java
@@ -1,20 +1,38 @@
 package com.ssafy.dubdub.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Builder
 public class RecruitmentSearchRequestDTO {
-    @Schema(description = "페이지 번호")
+    @Schema(description = "페이지 번호", example = "0")
     private Integer page;
+
+    @Schema(description = "한 페이지에 들어갈 갯수", example = "10")
     private Integer size;
+
+    @Schema(description = "스튜디오 생성된 / 정해진 시간인 onAir 모집글 조건", example = "true")
     private Boolean onAir;
+
+    @Schema(description = "비공개 여부 조건", example = "true")
     private Boolean isPrivate;
+
+    @Schema(description = "모집글 선택된 장르 조건", example = "[1, 2]")
     private List<Long> genreIds;
+
+    @Schema(description = "모집글 선택된 카테고리 조건", example = "[1, 2]")
     private List<Long> categoryIds;
+
+    @Schema(description = "모집글 모집중인 조건", example = "true")
     private Boolean isRecruiting;
+
+    @Schema(description = "전체 조회 / 내가 참여한 모집글 조건", example = "MY")
     private String participationType;
 }
 
