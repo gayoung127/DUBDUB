7332c7788647683591e19ff535b7c112f77c0314
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-02-07 14:25:02 +0900

    🐛 fix: cors mode 추가

--- src/app/_apis/user.ts
+++ src/app/_apis/user.ts
@@ -2,9 +2,16 @@ import { useUserStore } from "../_store/UserStore";
 
 export const getMyInfo = async () => {
   try {
-    const response = await fetch(`/api/getMyInfo`, {
+    const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+    if (!BASE_URL) {
+      console.error("백엔드 Url 환경 변수에서 못 찾아옴.");
+      return;
+    }
+
+    const response = await fetch(`${BASE_URL}/member/profile`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
+      mode: "cors",
       credentials: "include",
     });
 
