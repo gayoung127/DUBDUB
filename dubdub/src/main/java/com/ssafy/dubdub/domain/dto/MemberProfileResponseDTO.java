be850ea4e9b00649e90e12990610db4a9d1c5c1d
j2j2 <fineimagination@naver.com>
2025-02-07 11:37:52 +0900

    🔨Refactor: DTO패턴 정적메서드로 수정

--- dubdub/src/main/java/com/ssafy/dubdub/domain/dto/MemberProfileResponseDTO.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/dto/MemberProfileResponseDTO.java
@@ -3,30 +3,35 @@ package com.ssafy.dubdub.domain.dto;
 import com.ssafy.dubdub.domain.entity.Member;
 import com.ssafy.dubdub.enums.Position;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class MemberProfileResponseDTO {
     @Schema(description = "멤버 식별 id", example = "1")
-    private Long memberId;
+    private final Long memberId;
 
     @Schema(description = "멤버 email", example = "example@example.com")
-    private String email;
+    private final String email;
 
     @Schema(description = "멤버 역할", example = "PRO")
-    private Position position;
+    private final Position position;
 
     @Schema(description = "멤버 프로필 url", example = "s3업로드 된 주소")
-    private String profileUrl;
+    private final String profileUrl;
+
+    private MemberProfileResponseDTO(Long memberId, String email, Position position, String profileUrl) {
+        this.memberId = memberId;
+        this.email = email;
+        this.position = position;
+        this.profileUrl = profileUrl;
+    }
 
     public static MemberProfileResponseDTO from(Member member) {
-        return MemberProfileResponseDTO.builder()
-                .memberId(member.getId())
-                .email(member.getEmail())
-                .position(member.getPosition())
-                .profileUrl(member.getProfileUrl())
-                .build();
+        return new MemberProfileResponseDTO(
+                member.getId(),
+                member.getEmail(),
+                member.getPosition(),
+                member.getProfileUrl()
+        );
     }
 }
