cd644e02ac4342ac42ddc1a0aed6a4cecb5a7427
이주은 <68546023+lizuAg@users.noreply.github.com>
2025-02-11 02:41:26 +0900

    temp

--- dubdub/src/main/java/com/ssafy/dubdub/domain/dto/StudioEnterResponseDto.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/dto/StudioEnterResponseDto.java
@@ -3,11 +3,15 @@ package com.ssafy.dubdub.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Builder
 @Getter
 public class StudioEnterResponseDto {
     private final String title;
     private final String script;
+    private final List<String> roleList;
+    private final String videoUrl;
     private final String session;
     private final String token;
     private final ProjectSnapshot snapshot;
