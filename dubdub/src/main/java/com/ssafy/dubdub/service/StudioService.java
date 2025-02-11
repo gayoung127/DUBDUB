9ba156a62b0832ef6778ca84f98d2c0f2ea63607
이주은 <wndms058@gmail.com>
2025-02-11 10:41:56 +0900

    ✨ feat: 스튜디오 입장 정보

--- dubdub/src/main/java/com/ssafy/dubdub/service/StudioService.java
+++ dubdub/src/main/java/com/ssafy/dubdub/service/StudioService.java
@@ -1,18 +1,21 @@
 package com.ssafy.dubdub.service;
 
 import com.ssafy.dubdub.domain.dto.StudioEnterResponseDto;
+import com.ssafy.dubdub.domain.entity.File;
 import com.ssafy.dubdub.domain.entity.Member;
 import com.ssafy.dubdub.domain.entity.Recruitment;
 import com.ssafy.dubdub.domain.entity.Studio;
-import com.ssafy.dubdub.repository.RecruitmentRepository;
-import com.ssafy.dubdub.repository.StudioRepository;
+import com.ssafy.dubdub.enums.FileType;
+import com.ssafy.dubdub.repository.*;
 import io.openvidu.java.client.*;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,24 +25,36 @@ public class StudioService {
     private final OpenViduService openViduService;
     private final RecruitmentRepository recruitmentRepository;
     private final StudioRepository studioRepository;
+    private final FileRepository fileRepository;
+    private final CastingRepository castingRepository;
 
     public StudioEnterResponseDto createStudio(Member member, Long projectId) throws OpenViduJavaClientException, OpenViduHttpException {
-
         Recruitment project = recruitmentRepository.findById(projectId).orElseThrow(
                 () -> new NoSuchElementException("해당 프로젝트가 존재하지 않습니다.")
         );
 
-        Studio studio = studioRepository.findFirstByRecruitmentIdAndIsClosedIsFalse(projectId).orElse(
-            new Studio(project, openViduService.createSession())
-        );
+        List<String> roleList = castingRepository.findRoleNameListByRecruitmentId(projectId);
+
+        Optional<File> video = fileRepository.findByRecruitmentIdAndFileType(projectId, FileType.ORIGINAL_VIDEO);
+        String videoUrl = video.map(File::getUrl).orElse(null);
+
+        Studio studio = studioRepository.findFirstByRecruitmentIdAndIsClosedIsFalse(projectId)
+                .orElseGet(() -> {
+                    try {
+                        Studio newStudio = new Studio(project, openViduService.createSession());
+                        return studioRepository.save(newStudio);
+                    } catch (OpenViduJavaClientException | OpenViduHttpException e) {
+                        throw new RuntimeException("OpenVidu 세션 생성 중 오류 발생", e);
+                    }
+                });
 
         String token = openViduService.createConnection(studio.getSession());
-
-        studioRepository.save(studio);
 
         return StudioEnterResponseDto.builder()
                 .title(project.getTitle())
                 .script(project.getScript())
+                .videoUrl(videoUrl)
+                .roleList(roleList)
                 .token(token)
                 .session(studio.getSession())
                 .snapshot(null)
