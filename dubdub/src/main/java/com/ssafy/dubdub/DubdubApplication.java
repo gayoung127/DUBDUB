3c342197abdc46781129714cc1acd1ed981543bd
이주은 <wndms058@gmail.com>
2025-02-11 12:52:38 +0900

    chore: log aop 적용

--- dubdub/src/main/java/com/ssafy/dubdub/DubdubApplication.java
+++ dubdub/src/main/java/com/ssafy/dubdub/DubdubApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.dubdub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaAuditing
 @SpringBootApplication
 public class DubdubApplication {
