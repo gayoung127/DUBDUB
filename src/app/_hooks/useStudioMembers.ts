8554d3dc10214c65999ded566a200decf26373e0
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2025-02-12 10:33:30 +0900

    🐛 fix: RoleList 로직 self, otherMembers 분리

--- src/app/_hooks/useStudioMembers.ts
+++ src/app/_hooks/useStudioMembers.ts
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
-import { UserStore, useUserStore } from "../_store/UserStore";
 import useStompClient from "../_utils/socketClient";
 
+interface Member {
+  memberId: number;
+  email: string;
+  nickName: string;
+  position: string;
+  profileUrl: string;
+}
+
 export const useStudioMembers = () => {
-  const { memberId, email, nickName, position, profileUrl, setUser } =
-    useUserStore();
-  const [studioMembers, setStudioMembers] = useState<UserStore[]>([]);
+  const [studioMembers, setStudioMembers] = useState<Member[]>([]);
   const stompClientRef = useStompClient();
 
   useEffect(() => {
@@ -15,10 +20,10 @@ export const useStudioMembers = () => {
       console.log("📡 Subscribing to studio members...");
 
       const subscription = stompClientRef.current.subscribe(
-        "/topic/studioMembers",
+        "/topic/studioMembers", // 서버에서 발행하는 경로
         (message) => {
           try {
-            const data: UserStore[] = JSON.parse(message.body);
+            const data: Member[] = JSON.parse(message.body);
             setStudioMembers(data);
             console.log("🎭 Received Studio Members:", data);
           } catch (error) {
@@ -33,13 +38,14 @@ export const useStudioMembers = () => {
       };
     };
 
+    // STOMP 연결이 완료되었을 때 구독
     if (stompClientRef.current?.connected) {
       const unsubscribe = subscribeToMembers();
       return unsubscribe;
     }
 
     return;
-  }, [stompClientRef.current?.connected]);
+  }, [stompClientRef.current?.connected]); // STOMP 연결 여부가 변경될 때만 실행
 
   return studioMembers;
 };
