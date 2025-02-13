6c6b48dd88cb16e6d65a156d811c0674fbc8952b
이주은 <wndms058@gmail.com>
2025-02-13 11:06:03 +0900

    🐛 fix: /topic prefix 누락 에러 수정

--- dubdub/src/main/java/com/ssafy/dubdub/wss/controller/WebSocketController.java
+++ dubdub/src/main/java/com/ssafy/dubdub/wss/controller/WebSocketController.java
@@ -30,16 +30,15 @@ public class WebSocketController {
 
     //트랙 점유자(레코더) 공유
     @MessageMapping("/studio/{sessionId}/track/recorder")
-    @SendTo("/studio/{sessionId}/track/recorder")
+    @SendTo("/topic/studio/{sessionId}/track/recorder")
     public TrackRecorder broadcastTracks(@DestinationVariable String sessionId, TrackRecorder trackRecorder) {
-            studioStoreService.saveTrackRecorder(sessionId, trackRecorder);
-
+        studioStoreService.saveTrackRecorder(sessionId, trackRecorder);
         return trackRecorder;
     }
 
     //오디오 파일(에셋) 공유
     @MessageMapping("/studio/{sessionId}/asset")
-    @SendTo("/studio/{sessionId}/assets")
+    @SendTo("/topic/studio/{sessionId}/assets")
     public AudioAssetRequestDto broadcastAssets(@DestinationVariable String sessionId, AudioAssetRequestDto requestDto) {
         switch (requestDto.getAction()) {
             case SAVE -> {
@@ -55,7 +54,7 @@ public class WebSocketController {
 
     //트랙에 올라간 에셋(블록) 저장/공유
     @MessageMapping("/studio/{sessionId}/track/files")
-    @SendTo("/studio/{sessionId}/track/files")
+    @SendTo("/topic/studio/{sessionId}/track/files")
     public TrackAssetDto broadcastTracks(@DestinationVariable String sessionId, TrackAssetDto requestDto) {
 
         switch (requestDto.getAction()) {
