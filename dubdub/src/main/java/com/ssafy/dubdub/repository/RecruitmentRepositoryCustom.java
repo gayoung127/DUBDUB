603dd6e1532f33f48db9b1afdfe94a3d7e939a3e
이주은 <68546023+lizuAg@users.noreply.github.com>
2025-02-14 01:40:20 +0900

    🎨 chore: code format 개선

--- dubdub/src/main/java/com/ssafy/dubdub/repository/RecruitmentRepositoryCustom.java
+++ dubdub/src/main/java/com/ssafy/dubdub/repository/RecruitmentRepositoryCustom.java
@@ -2,7 +2,6 @@ package com.ssafy.dubdub.repository;
 
 import com.ssafy.dubdub.domain.dto.RecruitmentSearchRequestDTO;
 import com.ssafy.dubdub.domain.dto.RecruitmentWithVideoDto;
-import com.ssafy.dubdub.domain.dto.StudioEnterResponseDto;
 import com.ssafy.dubdub.domain.entity.Member;
 import com.ssafy.dubdub.domain.entity.Recruitment;
 import org.springframework.data.domain.Page;
@@ -11,5 +10,6 @@ import java.util.Optional;
 
 public interface RecruitmentRepositoryCustom {
     Page<Recruitment> findBySearchCondition(RecruitmentSearchRequestDTO condition, Member member);
+
     Optional<RecruitmentWithVideoDto> findRecruitmentWithVideo(Long recruitmentId);
 }
