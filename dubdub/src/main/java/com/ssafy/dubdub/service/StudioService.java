413c95cb945e17dbb5ee71d706b26bda2155e5da
이주은 <wndms058@gmail.com>
2025-02-13 15:54:09 +0900

    ✨ feat: 오디오에셋 업로드 api 구현

--- dubdub/src/main/java/com/ssafy/dubdub/service/StudioService.java
+++ dubdub/src/main/java/com/ssafy/dubdub/service/StudioService.java
@@ -1,19 +1,24 @@
 package com.ssafy.dubdub.service;
 
+import com.ssafy.dubdub.domain.dto.FileUploadResponseDTO;
 import com.ssafy.dubdub.domain.dto.StudioEnterResponseDto;
 import com.ssafy.dubdub.domain.entity.File;
 import com.ssafy.dubdub.domain.entity.Member;
 import com.ssafy.dubdub.domain.entity.Recruitment;
 import com.ssafy.dubdub.domain.entity.Studio;
 import com.ssafy.dubdub.enums.FileType;
+import com.ssafy.dubdub.exception.BaseException;
+import com.ssafy.dubdub.exception.ErrorCode;
 import com.ssafy.dubdub.repository.*;
 import com.ssafy.dubdub.domain.entity.WorkspaceData;
+import com.ssafy.dubdub.util.FileUtil;
 import io.openvidu.java.client.*;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -25,11 +30,13 @@ import java.util.Optional;
 public class StudioService {
 
     private final OpenViduService openViduService;
+    private final S3Service s3Service;
     private final RecruitmentRepository recruitmentRepository;
     private final StudioRepository studioRepository;
     private final FileRepository fileRepository;
     private final CastingRepository castingRepository;
     private final WorkspaceDataRepository workspaceDataRepository;
+    private final RecruitmentService recruitmentService;
 
     public StudioEnterResponseDto createStudio(Member member, Long projectId) throws OpenViduJavaClientException, OpenViduHttpException {
         Recruitment project = recruitmentRepository.findById(projectId).orElseThrow(
@@ -90,5 +97,27 @@ public class StudioService {
         if (!project.getAuthor().getId().equals(currentUser.getId())) {
             throw new AccessDeniedException("작업정보 접근 권한이 없습니다.");
         }
+    }
+
+    public FileUploadResponseDTO uploadAudioAsset(Member member, Long projectId, MultipartFile asset) {
+        if(!FileUtil.isValidAudioFile(asset)) {
+            throw new BaseException(ErrorCode.INVALID_AUDIO_FORMAT);
+        }
+        String filePath = FileUtil.generateFilePath(member.getEmail(), FileType.AUDIO);
+        String url = s3Service.uploadFile(asset, filePath);
+
+        Recruitment recruitment = recruitmentRepository.findById(projectId).orElseThrow(
+                () -> new EntityNotFoundException("프로젝트를 찾을 수 없습니다.")
+        );
+
+        File file = File.builder()
+                .url(url)
+                .recruitment(recruitment)
+                .fileType(FileType.AUDIO)
+                .build();
+
+        fileRepository.save(file);
+
+        return new FileUploadResponseDTO(url, recruitment.getId());
     }
 }
