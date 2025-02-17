8b94f89d56c51c9a6556627f658c9401f0a5890f
이주은 <wndms058@gmail.com>
2025-02-17 09:59:39 +0900

    🎨 refactor: 필요 없는 import 제거

--- dubdub/src/main/java/com/ssafy/dubdub/wss/dto/AudioAsset.java
+++ dubdub/src/main/java/com/ssafy/dubdub/wss/dto/AudioAsset.java
@@ -1,17 +1,9 @@
 package com.ssafy.dubdub.wss.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.redis.core.RedisHash;
 
-import java.io.Serializable;
-
-//@RedisHash("AudioAsset")
-//@AllArgsConstructor
-//@NoArgsConstructor
 @Getter
-public class AudioAsset implements Serializable {
+public class AudioAsset {
     private String id;
     private long tableId;
     private float duration;
