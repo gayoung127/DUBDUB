d47ccf37e67202ba87e3f8faf0d096581c2b7833
이주은 <wndms058@gmail.com>
2025-02-18 12:20:00 +0900

    🐛 fix: 녹음여부(isRecording -> recording)) 매핑 오류 수정

--- dubdub/src/main/java/com/ssafy/dubdub/wss/dto/PlaybackStatus.java
+++ dubdub/src/main/java/com/ssafy/dubdub/wss/dto/PlaybackStatus.java
@@ -1,12 +1,16 @@
 package com.ssafy.dubdub.wss.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class PlaybackStatus {
-    private final boolean isRecording;
+
+    private final boolean recording;
+
     private final String playState; // "PLAY", "PAUSE", "STOP"
+
     private final double timelineMarker; // 현재 타임라인 위치 (초)
 }
