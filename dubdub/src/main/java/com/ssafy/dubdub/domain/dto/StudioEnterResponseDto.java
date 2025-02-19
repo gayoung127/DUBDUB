39d0eb1e8f1a9e3ae49d1c736c46faff8479fe1f
이주은 <wndms058@gmail.com>
2025-02-19 22:05:53 +0900

    ✨ feat: script json 직렬화/역직렬화 적용

--- dubdub/src/main/java/com/ssafy/dubdub/domain/dto/StudioEnterResponseDto.java
+++ dubdub/src/main/java/com/ssafy/dubdub/domain/dto/StudioEnterResponseDto.java
@@ -1,5 +1,8 @@
 package com.ssafy.dubdub.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.ssafy.dubdub.util.JsonStringDeserializer;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,11 +12,20 @@ import java.util.List;
 @Getter
 public class StudioEnterResponseDto {
     private final String title;
+
+    @JsonRawValue
+    @JsonDeserialize(using = JsonStringDeserializer.class)
     private final String script;
+
     private final List<String> roleList;
+
     private final String videoUrl;
+
     private final String thumbnailUrl;
+
     private final String session;
+
     private final String token;
+
     private final SnapshotDTO snapshot;
 }
