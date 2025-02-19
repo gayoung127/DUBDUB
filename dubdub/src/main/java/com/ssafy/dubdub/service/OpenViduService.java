735fad6781a1bced1c1c003b5cb2a6c81dae4d3f
이주은 <wndms058@gmail.com>
2025-02-19 20:37:40 +0900

    ♻️ refactor: activeSessions map 삭제

--- dubdub/src/main/java/com/ssafy/dubdub/service/OpenViduService.java
+++ dubdub/src/main/java/com/ssafy/dubdub/service/OpenViduService.java
@@ -5,8 +5,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 @Transactional
 @RequiredArgsConstructor
 @Service
@@ -16,21 +14,17 @@ public class OpenViduService {
     private final SessionProperties sessionProperties;
     private final ConnectionProperties connectionProperties;
 
-    private final ConcurrentHashMap<String, Session> activeSessions = new ConcurrentHashMap<>();
-
     public String createSession() throws OpenViduJavaClientException, OpenViduHttpException {
 
         Session session = openVidu.createSession(sessionProperties);
-        activeSessions.put(session.getSessionId(), session);
+
         return session.getSessionId();
     }
 
     public String createConnection(String sessionId) throws OpenViduJavaClientException, OpenViduHttpException {
-        if (!activeSessions.containsKey(sessionId)) {
-            throw new IllegalArgumentException("세션이 존재하지 않습니다.");
-        }
 
-        Session session = activeSessions.get(sessionId);
+        Session session = openVidu.getActiveSession(sessionId);
+
         Connection connection = session.createConnection(connectionProperties);
 
         return connection.getToken();
