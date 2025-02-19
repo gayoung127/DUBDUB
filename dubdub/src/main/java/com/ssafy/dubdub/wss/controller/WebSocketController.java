480b8509cefe9238e1e93b67d8593c78693367ce
이현정 <fineimagination@naver.com>
2025-02-19 14:03:40 +0900

    Update dubdub/src/main/java/com/ssafy/dubdub/wss/controller/WebSocketController.java

--- dubdub/src/main/java/com/ssafy/dubdub/wss/controller/WebSocketController.java
+++ dubdub/src/main/java/com/ssafy/dubdub/wss/controller/WebSocketController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
